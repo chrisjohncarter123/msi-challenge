6274937a503f93fc4ab3aa2f4593be99e8ed2cb2
Chris Carter <chrisjohncarter123@gmail.com>
2020-12-23 12:32:25 -0600

    calculate function splits and displays input string

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,18 @@
 function calculate(){
     var resultDiv = document.getElementById("result");
 
+    var userInput = document.getElementById("userInput").value
+
+    var nodes = userInput.split("|")
+
+    var resultHTML = ""
+
+    var coutner = 0
+
+    nodes.forEach(element => {
+        resultHTML += "<p>Node " + ++coutner + ": " + element + "</p>"
+    });
+
+    resultDiv.innerHTML = resultHTML
+
 }
