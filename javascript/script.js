4f557fd1319c4175777d4e401fe6dd65f2de715d
Chris Carter <chrisjohncarter123@gmail.com>
2020-12-23 13:36:51 -0600

    Added tree and node classes

--- javascript/script.js
+++ javascript/script.js
@@ -1,58 +1,52 @@
-
 function onClick(){
 
     var userInput = document.getElementById("userInput").value
 
-    var inputObject = inputToObject(userInput)
+    var tree = new Tree(userInput)
 
-    display(userInput)
+    console.log(tree)
+
+   // tree.display()
 
 }
 
-function inputToObject (userInput){
+class Tree{
+    constructor(userInput){
+        var userInputNodes = userInput.split("|")
+        this.nodes = []
 
-    var nodes = userInput.split("|")
+        userInputNodes.forEach(element => {
+            var values = element.split(",")
 
-    var result = []
-
-    nodes.forEach(element => {
-        var values = element.split(",")
-
-        result.push(
-        {
-            "parent_id":values[0],
-            "node_id":values[1],
-            "node_name":values[2]
+            this.nodes.push(new Node(values[0],values[1],values[2]))
         })
-    })
+    }
 
+    display() {
+        var resultDiv = document.getElementById("result");
 
-    //console.log(result)
+        var resultHTML = ""
 
-    return result
+        this.nodes.forEach(element => {
 
+            resultHTML += "<p>Node " + coutner++ + ":"
+
+            var items = element.split(",")
+
+            resultHTML += "<p>parent_id - " + items[0] + "</p>"
+            resultHTML += "<p>node_id - " + items[1] + "</p>"
+            resultHTML += "<p>node_name - " + items[2] + "</p>"
+            
+        });
+
+        resultDiv.innerHTML = resultHTML
+    }
 }
 
-function display(nodesObject){
-    
-    var resultDiv = document.getElementById("result");
-
-    var resultHTML = ""
-    var coutner = 0
-    var nodes = nodesObject.split("|")
-
-    nodes.forEach(element => {
-
-        resultHTML += "<p>Node " + coutner++ + ":"
-
-        var items = element.split(",")
-
-        resultHTML += "<p>parent_id - " + items[0] + "</p>"
-        resultHTML += "<p>node_id - " + items[1] + "</p>"
-        resultHTML += "<p>node_name - " + items[2] + "</p>"
-        
-    });
-
-    resultDiv.innerHTML = resultHTML
-
+class Node{
+    constructor(parent_id, node_id, node_name){
+        this.parent_id = parent_id;
+        this.node_id = node_id;
+        this.node_name = node_name;
+    }
 }
